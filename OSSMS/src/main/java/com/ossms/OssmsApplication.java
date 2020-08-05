d7fd9bb2eb0bf853d9683b5a3db77c6285d83f5f
Nadun <dilhannadun777@gmail.com>
2020-08-05 10:27:29 +0530

    First Commit - Empty project

---
+++ OSSMS/src/main/java/com/ossms/OssmsApplication.java
@@ -0,0 +1,13 @@
+package com.ossms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssmsApplication.class, args);
+	}
+
+}
---
+++ OSSMS/src/test/java/com/ossms/OssmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
