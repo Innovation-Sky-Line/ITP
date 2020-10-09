11c3abb4f8aea58c36687cd88ecfc40acf802bd6
Nadun <dilhannadun777@gmail.com>
2020-10-09 21:34:50 +0530

    home and few more

--- OSSMS/src/main/java/com/ossms/controller/ProductController.java
+++ OSSMS/src/main/java/com/ossms/controller/ProductController.java
@@ -4,8 +4,13 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.engine.query.spi.sql.NativeSQLQueryCollectionReturn;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +28,12 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.lowagie.text.DocumentException;
+import com.ossms.model.DiscountExportPDF;
 import com.ossms.model.ProductCategoryModel;
 import com.ossms.model.ProductList;
 import com.ossms.model.ProductModel;
+import com.ossms.model.ProductPDFexporter;
 import com.ossms.model.Supplier;
 import com.ossms.service.ProductService;
 
@@ -477,6 +485,13 @@ public class ProductController {
 			pList.add(list);
 		}
 		mv.addObject("insufficient", pList);
+		return mv;
+	}
+	
+
+	@RequestMapping(value="/disProReport", method=RequestMethod.GET)
+	 public ModelAndView disProReport() {
+		ModelAndView mv = new ModelAndView("ProManage/DisReports");
 		
 		List<ProductModel> p = productService.getDiscountProducts();
 		List<ProductList> pList2 = new ArrayList<ProductList>();
@@ -489,6 +504,58 @@ public class ProductController {
 		}
 		mv.addObject("discounted", pList2);
 		return mv;
+	}
+	
+	@RequestMapping("/exportProductPdf")
+	public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
+		response.setContentType("application/pdf");
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");
+		String currentDateTimeString = dateFormat.format(new Date());
+		
+		String headerKeyString = "Content-Disposition";
+		String headerValue = "attachment; filename=Insufficient_products_"+currentDateTimeString+".pdf";
+		response.setHeader(headerKeyString, headerValue);
+		
+		List<ProductModel> products = productService.getInsufficientProducts();
+		List<ProductList> pList = new ArrayList<ProductList>();
+		for(ProductModel productModel : products) {
+			ProductCategoryModel categoryModel = productService.getCategoryById(productModel.getCategoryId());
+			Supplier supplier = productService.getSupplierById(productModel.getSupplierId());
+			ProductList list = new ProductList(productModel, categoryModel, supplier);
+			
+			pList.add(list);
+		}
+		
+		ProductPDFexporter productPDFexporter = new ProductPDFexporter(pList);
+		productPDFexporter.export(response);
+		
+	}
+	
+	@RequestMapping("/disExportProductPdf")
+	public void disExportToPDF(HttpServletResponse response) throws DocumentException, IOException {
+		response.setContentType("application/pdf");
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");
+		String currentDateTimeString = dateFormat.format(new Date());
+		
+		String headerKeyString = "Content-Disposition";
+		String headerValue = "attachment; filename=Discounted_products_"+currentDateTimeString+".pdf";
+		response.setHeader(headerKeyString, headerValue);
+		
+		List<ProductModel> p = productService.getDiscountProducts();
+		List<ProductList> pList2 = new ArrayList<ProductList>();
+		for(ProductModel productModels : p) {
+			ProductCategoryModel categoryModel2 = productService.getCategoryById(productModels.getCategoryId());
+			Supplier supplier2 = productService.getSupplierById(productModels.getSupplierId());
+			ProductList list2 = new ProductList(productModels, categoryModel2, supplier2);
+			
+			pList2.add(list2);
+		}
+		
+		DiscountExportPDF discountExportPDF = new DiscountExportPDF(pList2);
+		discountExportPDF.export(response);
+		
 	}
 	
 }
